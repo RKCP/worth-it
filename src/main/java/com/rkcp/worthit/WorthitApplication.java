29a34ae5a27e2e8cb76a07ea9209e78b4d24fc32
RAPHAEL PETERS <RKCPETERS@gmail.com>
2026-05-04 14:11:35 +0100

    Initial commit

---
+++ src/main/java/com/rkcp/worthit/WorthitApplication.java
@@ -0,0 +1,13 @@
+package com.rkcp.worthit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorthitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorthitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkcp/worthit/WorthitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkcp.worthit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorthitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
